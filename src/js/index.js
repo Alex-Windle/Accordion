caa9061f2cac459e440e9cb925bf5db1d8472af7
Alex Windle <alexwindle85@gmail.com>
2016-03-28 16:52:10 -0400

    added .removeClass() behavior

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 // Javascript Entry Point
+import $ from 'jquery';
+
+$('h3').on('click', function(event){
+
+	$('section').addClass('hidden');
+
+	var h3=event.currentTarget;
+	var $h3=$(h3);
+
+	$h3.parent().removeClass('hidden');
+
+});
